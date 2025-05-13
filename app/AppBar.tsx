42abf74d9bd773f558c8eb82f6d8263afd7442df
Abed <abedkansof18@gmail.com>
2025-05-13 19:06:09 +0300

    Last Commit

---
+++ app/AppBar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const AppBar = () => {
+  return (
+    <div className="bg-gradient-to-b from-cyan-50 to-cyan-200 p-4 flex gap-5 ">
+
+    </div>
+  );
+};
+
+export default AppBar;
---
+++ app/types/user.ts
@@ -0,0 +1,13 @@
+export interface ProfileData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+}
+
+export interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
